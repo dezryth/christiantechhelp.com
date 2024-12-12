a34d1354dbf745ad7df7b0e5116bc234a8cd2edb
Scott Christian <scott@codesilver.io>
2024-12-11 23:35:07 -0600

    A few updates to verbiage and added phone number.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://christiantechhelp.com',
-  base: '/christiantechhelp.com',
+  // base: '/christiantechhelp.com',
   outDir: './docs', // Change the build output folder to 'docs'
 	integrations: [mdx(), sitemap()],
 });
