b8e01b67a46c94db8c7d6922c0a0bc833702cb35
Scott Christian <scott@codesilver.io>
2024-12-12 14:20:14 -0600

    Added lil computer icon to SITE_TITLE

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Christian Tech Help';
+export const SITE_TITLE = '💻Christian Tech Help';
 export const SITE_DESCRIPTION = 'Neighborly Help with Technology at an affordable price!';
