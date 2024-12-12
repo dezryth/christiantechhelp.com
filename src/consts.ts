d9d9a4efe8766ca29b51ea8434cdefa2708db8a4
Scott Christian <scott@codesilver.io>
2024-12-11 22:24:40 -0600

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Christian Tech Help';
+export const SITE_DESCRIPTION = 'Neighborly Help with Technology at an affordable price!';
